0f6f1f2e9f30231c706c49213226ccada024ea7d
A1n2F <alissonfferreira22@gmail.com>
2025-03-27 10:34:56 -0300

    Desacoplando camadas no NestJS - Testes do cadastro e autenticação

---
+++ test/factories/make-student.ts
@@ -0,0 +1,18 @@
+import { Student, StudentProps } from "@/domain/forum/enterprise/entities/student";
+import { faker } from "@faker-js/faker"
+
+import { UniqueEntityID } from "src/core/entities/unique-entity-id";
+
+export function makeStudent(
+    override: Partial<StudentProps> = {},
+    id?: UniqueEntityID
+) {
+    const student = Student.create({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        ...override
+    }, id)
+
+    return student
+}
